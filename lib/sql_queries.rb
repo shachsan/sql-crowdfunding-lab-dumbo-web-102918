e415929c0ee8408bb7911fdad7dc93a1be9c1711
SANJAY K SAI <shachsan@hotmail.com>
2018-11-05 21:45:54 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING 
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.a)"
+"SELECT users.name, SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
