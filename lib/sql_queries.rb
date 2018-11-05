e6fd85f7fec592f4b65496a6bab6ca5e9d0ec827
SANJAY K SAI <shachsan@hotmail.com>
2018-11-05 22:10:32 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,5 +32,5 @@ JOIN pledges ON projects.id = pledges.project_id WHERE projects.category='music'
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.category, "
+"SELECT projects.category, SUM9"
 end
